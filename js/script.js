6d385252a9f392d35267fec66ba9da7f1e587994
Anton Slizhou <sonmil.100@gmail.com>
2025-06-14 23:22:52 +0300

    final 1.0.0

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
+require('es6-promise').polyfill();
+import 'nodelist-foreach-polyfill';
+
 import tabs from './modules/tabs';
 import modal from './modules/modal';
-import timer from './modules/timer';
+import timer from './modules/timer'; 
 import cards from './modules/cards';
 import calc from './modules/calc';
 import forms from './modules/forms';
